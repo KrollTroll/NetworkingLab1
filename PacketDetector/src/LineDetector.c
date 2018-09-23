e5ebc5e84f7401a9ee1d418d9f4ce01e9e5184b0
Connor Kroll <31713317+KrollTroll@users.noreply.github.com>
2018-09-23 13:46:08 -0500

    Further LED Control Implemented
    updated LED control to reflect individual control

--- PacketDetector/src/LineDetector.c
+++ PacketDetector/src/LineDetector.c
@@ -7,11 +7,10 @@
 #include "GPIO.h"
 
 int main(){
-	init_led();
-	*(GPIOA_ODR) |= (0b111<<5);
+	init_led_pins();
 }
 
-void init_led()
+void init_led_pins()
 {
 	//enable RCC for GPIOA
 	*(RCC_AHB1ENR) |= (1<<GPIOAEN);
@@ -20,3 +19,34 @@ void init_led()
 	*(GPIOA_MODER) |= (0b010101<<10);
 	}
 
+void init_input_pin()
+{
+	//set PA3 for input
+	*(GPIOA_MODER) &= (0xFFFFFF3F);
+	}
+
+void set_LED(char position)
+{
+	if(position == 'r')
+	{
+		*(GPIOA_ODR) |= (0b1<<5);
+	}
+	else if(position == 'y')
+	{
+		*(GPIOA_ODR) |= (0b1<<6);
+	}
+	else if(position == 'g')
+	{
+		*(GPIOA_ODR) |= (0b1<<7);
+	}
+	}
+
+void set_LEDS()
+{
+	*(GPIOA_ODR) |= (0b111<<5);
+	}
+
+void clear_LEDS()
+{
+	*(GPIOA_ODR) &= (0x00000000);
+	}
